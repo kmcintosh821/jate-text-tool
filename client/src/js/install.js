0cd1efe046245422952b86cbed1f37bec8377807
kmcintosh821 <115330642+BittersweetNSour@users.noreply.github.com>
2023-11-11 22:08:00 -0500

    Updated install.js

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,11 +1,32 @@
 const butInstall = document.getElementById('buttonInstall');
 
+let installPrompt;
+
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+window.addEventListener('beforeinstallprompt', (event) => {
+    event.preventDefault();
+    installPrompt = event;
+    butInstall.style.display = 'block';
+});
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+butInstall.addEventListener('click', async () => {
+    if (!installPrompt) {
+        return;
+    }
+    const result = await installPrompt.prompt();
+    console.log(`Response: ${result.outcome}`);
+
+    disableInAppInstall();
+});
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+window.addEventListener('appinstalled', (event) => {
+    disableInAppInstall();
+});
+
+function disableInAppInstall() {
+    installPrompt = null;
+    butInstall.setAttribute("hidden", "")
+}
